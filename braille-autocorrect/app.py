1c46df510426ff065cd37a3617503067e5af4a48
ameenmohammedcs50 <61346774+ameenmohammedcs50@users.noreply.github.com>
2025-05-17 15:16:37 +0530

    Update app.py

--- braille-autocorrect/app.py
+++ braille-autocorrect/app.py
@@ -89,6 +89,7 @@ def suggest():
     })
 
 if __name__ == '__main__':
-    # Allow access from all IPs (important for deployment or non-local use)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
